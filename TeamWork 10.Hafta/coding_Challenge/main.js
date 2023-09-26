7cc005a8eb29993a3410c15837a69b249333f826
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-26 15:17:51 +0200

    25.09.2023 TW10 ready

--- TeamWork 10.Hafta/coding_Challenge/main.js
+++ TeamWork 10.Hafta/coding_Challenge/main.js
@@ -19,42 +19,56 @@ const products = [
     { name: "Product 6", price: 70, category: "Electronics" },
     { name: "Product 7", price: 80, category: "Clothes" },
     { name: "Product 8", price: 90, category: "Electronics" },
+    { name: "Product 9", price: 40, category: "Home" },
+    { name: "Product 10", price: 40, category: "Home" },
 ];
-// Adım 1: Kategorilere göre ürünleri gruplayın
-const groupedProducts = {};
-products.forEach(product => {
-    if (!groupedProducts[product.category]) {
-        groupedProducts[product.category] = [];
-    }
-    groupedProducts[product.category].push(product);
+
+const clothes = products.filter((a) => a.category === "Clothes")
+const a = clothes.reduce((toplam, a) => toplam + a.price, 0) / clothes.length
+
+const electronics = products.filter((a) => a.category === "Electronics")
+const b = electronics.reduce((toplam, a) => toplam + a.price, 0) / electronics.length
+
+const home = products.filter((a) => a.category === "Home")
+const c = home.reduce((toplam, a) => toplam + a.price, 0) / home.length
+
+let sonuc = []
+sonuc.push(a)
+sonuc.push(b)
+sonuc.push(c)
+console.log(sonuc);
+
+sonuc = sonuc.filter((a) => a > 50)
+console.log(sonuc);
+
+//* -------------------------------------------------------------------------- */
+
+/* Use map to create an object with category as the key
+and an array of products as the value */
+const productsByCategory = products.reduce((acc, product) => {
+    const category = product.category;
+    if (!acc[category]) {
+        acc[category] = [];
+    } // else yazarsa ilki ni almiyor o yuzden else kullanmiyoruz
+    acc[category].push(product);
+    return acc;
+}, {}); // burda sus lu obje seklinde dondurmek icin kullaniliyor
+console.log(productsByCategory);
+
+
+console.log(Object.keys(productsByCategory));
+// Use map to calculate the average price for each category
+const avgPriceByCategory = Object.keys(productsByCategory).map(category => {
+    const sum = productsByCategory[category].reduce((acc, product) => acc + product.price, 0);
+    console.log(sum); // 3 ayri toplam oluyor
+    return {
+        category: category, average: sum / productsByCategory[category].length
+    };
 });
-// Adım 2: Ortalama fiyatları hesaplayın
-const categoryAverages = [];
-Object.keys(groupedProducts).forEach(category => {
-    const productsInCategory = groupedProducts[category];
-    const totalPrice = productsInCategory.reduce((sum, product) => sum + product.price, 0);
-    const averagePrice = totalPrice / productsInCategory.length;
-    categoryAverages.push({ category, average: averagePrice });
-});
-// Adım 3: Ortalama fiyatı 50'nin üstündeki kategorileri filtreleyin
-const result = categoryAverages.filter(categoryAverage => categoryAverage.average > 50);
-
-console.log(result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(avgPriceByCategory);
+// Use filter to only select categories with an average above a certain threshold
+const highPricedCategories = avgPriceByCategory.filter(category => category.average > 50);
+console.log(highPricedCategories)
 
 
 
@@ -75,4 +89,27 @@ const employees = [
     { name: "Mike", salary: 65000, department: "IT" },
     { name: "Emily", salary: 80000, department: "HR" },
     { name: "David", salary: 70000, department: "IT" },
+    // { name: "Saban", salary: 100000, department: "IT" },
 ];
+
+/* Use reduce to create an object with department as the key
+and an array of employee objects as the value */
+const employeesByDepartment = employees.reduce((acc, employee) => {
+    const department = employee.department;
+    if (!acc[department]) {
+        acc[department] = [];
+    }
+    acc[department].push(employee);
+    return acc;
+}, {});
+
+// Use map to calculate the average salary for each department
+const avgSalaryByDepartment = Object.keys(employeesByDepartment).map(department => {
+    const sum = employeesByDepartment[department].reduce((acc, employee) => acc + employee.salary, 0);
+    return {
+        department: department, average: (sum / employeesByDepartment[department].length).toFixed()
+    };
+});
+// Use filter to only select departments with an average above a certain threshold
+const highPaidDepartments = avgSalaryByDepartment.filter(department => department.average > 65000);
+console.log(highPaidDepartments)
