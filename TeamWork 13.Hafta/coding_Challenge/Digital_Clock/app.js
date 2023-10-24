f6aca0817f3ec87281486427ac18992af68696cc
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-24 23:28:06 +0200

    TW-13 Digital Clock Start

--- TeamWork 13.Hafta/coding_Challenge/Digital_Clock/app.js
+++ TeamWork 13.Hafta/coding_Challenge/Digital_Clock/app.js
@@ -8,6 +8,8 @@ function startTime() {
     let s = today.getSeconds();
     let session = "AM";
     
+
+    // eğer saat 0'a eşitse, 12'ye dönüştürür. Ardından yine eğer saat 12'den büyükse, saatten 12 çıkarılır ve 'session' değişkenini "PM" olarak ayarladim.
     if (h == 0) {
         h = 12;
     }
@@ -19,11 +21,14 @@ function startTime() {
     
     m = checkTime(m);
     s = checkTime(s);
-    
+
     document.getElementById('txt').innerHTML = h + ":" + m + ":" + s + " " + session;
 
+    // her 1 saniyede bir kendini tekrarlamasını sağladim, böylece saati her saniye güncelliyorum
     setTimeout(startTime, 1000);
 }
+
+// Bu fonksiyon ile verilen değerin 10'dan küçük olup olmadığını kontrol ediyorum ve gerektiğinde başına sıfır ekliyorum
 
 function checkTime(i) {
     if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
