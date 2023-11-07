b67a3a87af5da01959deb0d21e934c42fd3640db
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-07 13:15:54 +0100

    07.11.2023 todoAppSelfProject withReact mid update

--- TeamWork 15.Hafta/case_study&Project/To-Do App/src/components/List_Item/ListItem.jsx
+++ TeamWork 15.Hafta/case_study&Project/To-Do App/src/components/List_Item/ListItem.jsx
@@ -1,12 +1,25 @@
 
 import "../List_Item/ListItem.css"
 
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 const ListItem = () => {
   return (
 
-    <div className='list_item text-white'>
+    <div className='list'>
 
-      
+      <div className="todo_radio">
+        <i class="bi bi-check-lg"></i>
+      </div>
+
+      <div className="todo_text">
+        Testing Text
+      </div>
+
+      <div>
+        <i class="bi bi-trash"></i>
+      </div>
+
 
     </div>
   )
