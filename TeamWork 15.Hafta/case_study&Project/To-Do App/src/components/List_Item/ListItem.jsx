18fb3e88d9bc6cadf0072e0b376dfc4876a2990e
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-08 01:35:08 +0100

    08.11.2023 todoAppSelfProject withReact mid end

--- TeamWork 15.Hafta/case_study&Project/To-Do App/src/components/List_Item/ListItem.jsx
+++ TeamWork 15.Hafta/case_study&Project/To-Do App/src/components/List_Item/ListItem.jsx
@@ -1,22 +1,24 @@
 
+
 import "../List_Item/ListItem.css"
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-const ListItem = () => {
+const ListItem = ({ text, deleteTodo, isChecked, toggleChecked }) => {
+
   return (
 
     <div className='list'>
 
-      <div className="todo_radio">
-        <i class="bi bi-check-lg"></i>
+      <div className={`${isChecked ? "todo_radio" : "todo_radio_unchecked"} `} onClick={toggleChecked}>
+        {isChecked && <i class="bi bi-check-lg"></i>}
       </div>
 
-      <div className="todo_text">
-        Testing Text
+      <div className={`${isChecked ? "todo_text_checked" : "todo_text"}`}>
+        {text}
       </div>
 
-      <div>
+      <div className="todo_delete" onClick={deleteTodo}>
         <i class="bi bi-trash"></i>
       </div>
 
--- TeamWork 15.Hafta/case_study&Project/To-Do App/src/components/TodoList.jsx
+++ TeamWork 15.Hafta/case_study&Project/To-Do App/src/components/TodoList.jsx
@@ -1,6 +1,9 @@
+
 import { v4 as uuidv4 } from 'uuid';
 
 import ListItem from './List_Item/ListItem';
+
+import { useEffect, useState } from "react";
 
 import { Button, Form, InputGroup } from 'react-bootstrap';
 
@@ -8,26 +11,95 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 
 
-
-
 const TodoList = () => {
+
+    const [todo, setTodo] = useState("")
+    const [allTodos, setAllTodos] = useState([])
+
+    const addTodo = (e) => {
+
+        e.preventDefault()
+
+        const todoItem = {
+            // id: new Date().getTime(),
+            id: uuidv4(),
+            text: todo,
+            isChecked: false
+        }
+
+        if (todo !== "") {
+            setAllTodos([...allTodos].concat(todoItem).reverse())
+            setTodo("")
+        }
+
+        console.log(allTodos);
+
+    }
+
+    const getAllTodos = () => {
+        let stored = JSON.parse(localStorage.getItem("todo"))
+
+        if (stored) {
+            setAllTodos(stored)
+        }
+    }
+
+    const toggleChecked = (id) => {
+        let updatedTodos = [...allTodos].map((todo) => {
+            if (todo.id === id) {
+                todo.isChecked = !todo.isChecked
+            }
+
+            return todo
+        })
+        setAllTodos(updatedTodos)
+    }
+
+    const deleteTodo = (id) => {
+        const filteredTodo = allTodos.filter((todo) => todo.id !== id)
+        setAllTodos(filteredTodo)
+    }
+
+    useEffect(() => {
+        getAllTodos()
+    }, [])
+
+
+    useEffect(() => {
+        localStorage.setItem("todo", JSON.stringify(allTodos))
+    }, [allTodos])
+
+
     return (
         <div className='todo m-auto'>
-            <InputGroup className="todo_wrapper mt-5 mb-4">
+            <InputGroup className="todo_wrapper mt-5 mb-4" onSubmit={addTodo}>
                 <Form.Control
                     className='input text-white border-0 '
                     placeholder="Create new todo..."
-                    type='text' />
+                    type='text'
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                />
 
-                <Button className='but text-white ' variant="outline-secondary" id="button-addon2">
+                <Button className='but text-white ' variant="outline-secondary" id="button-addon2" onClick={addTodo}>
                     <i class="bi bi-arrow-bar-down" ></i>
                 </Button>
             </InputGroup>
 
             <div className='todo_list'>
-                <ListItem />
-            </div>
+                {
+                    allTodos.map((todo) => (
+                        <ListItem key={todo.id} deleteTodo={() => deleteTodo(todo.id)} text={todo.text} isChecked={todo.isChecked} toggleChecked={() => toggleChecked(todo.id)} />
+                    ))
+                }
 
+                {
+
+                    allTodos.length === 0 && (
+                        <p className='empty'>There are no Todo's</p>
+                    )
+                }
+            </div>
 
         </div>
     )
