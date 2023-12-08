5198e67d79438449a0dddbf60a75bfb4d2f35d69
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-12-08 13:48:26 +0100

    TW19-CihanBag Shopping ready to react and redux update

--- TeamWork 19.Hafta/Clarus-Shopping/src/pages/Home.jsx
+++ TeamWork 19.Hafta/Clarus-Shopping/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
+import CategoryBar from "../components/CategoryBar";
 
 const Home = () => {
   return (
     <div>
+      <CategoryBar />
       <ProductCard />
     </div>
   );
